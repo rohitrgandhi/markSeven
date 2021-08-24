2203f07dcce826521773cd752ab382b269c6e30f
Rohit Gandhi <rohit.r.gandhi@gmail.com>
2021-08-24 19:43:50 +0530

    Done

---
+++ app.js
@@ -0,0 +1,28 @@
+// button
+var transButton = document.querySelector("#translate-button");
+transButton.addEventListener("click", buttonClickHandler)
+// take input
+var translateInput = document.querySelector("#translate-input");
+
+// output
+var translateOutput = document.querySelector("#translate-output");
+
+var url = "	https://api.funtranslations.com/translate/pirate.json"
+function buttonClickHandler(event)
+{
+    console.log("button clicked");
+    var input = translateInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            translateOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+}
+
+function constructURL(inputText) {
+    var translatedURL = encodeURI(inputText);
+    return url+"?text="+translatedURL;
+}
